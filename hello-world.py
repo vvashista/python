fdacbf3281c0ecdf04d76ca8f15577696d328dd7
Vishal Vashista <vishal.vashista1311@gmail.com>
2019-02-03 01:13:08 -0500

    Completed basic Pythin syntax, started string manipulation with string.py

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,18 @@
 print("Hello world")
 #this is a comment
+"""Doc String"""
 if 5 > 2:
-    print("Five is greater than 2")
+ print("Five is greater than 2")
+x = 5
+y = "john"
+print(x)
+print(y)
+print(x+x)
+print(y+" "+y)
+# print(x + y) gives TypeError: unsupported operand types
+x1=1
+x2=1.2
+x3=1j
+print(type(x1))
+print(type(x2))
+print(type(x3))
