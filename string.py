d3e47103a5120c7ee3a3ee3d0da65cd79c3bca18
Vishal Vashista <vishal.vashista1311@gmail.com>
2019-02-03 01:53:08 -0500

    String manipulation with string.py

---
+++ string.py
@@ -0,0 +1,8 @@
+a= "Hello, World..."
+b="\nIn python, string literals are stored as an array. For example, a[0]= " + a[0]
+c="\na[2:5] =" + a[2:5] + " (Substring of length 3 and not 4)"
+d="\na.strip() will strip trailing and leading white spaces\nlen(a) gives length"
+e="\na.lower()\na.upper()\na.replace(\"H\",\"J\")\na.split(\",\")"
+print(a+b+c+d+e);
+x=input()
+print(x)
